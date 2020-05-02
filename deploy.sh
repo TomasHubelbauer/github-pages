9c1063df7e3aef6d427a05d4462ba981eb5823eb
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-05-02 09:58:52 +0200

    Demonstrate a GitHub Pages build watcher script

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+# Print the commands as they are being executed and fail or error
+set +x
+set +e
+
+# Read the GitHub API PAT capable of accessing the GitHub Pages APIs
+source token.sh
+authorization="Authorization: token $token"
+accept="Accept: application/vnd.github.mister-fantastic-preview+json"
+
+pagesBuildsUrl="https://api.github.com/repos/tomashubelbauer/musicblackholes.com/pages/builds"
+pagesBuildsJson="pages-builds.json"
+
+# Enqueue a GitHub Pages deployment using the API with the PAT
+curl -s -f -X POST -H "$authorization" -H "$accept" $pagesBuildsUrl > $pagesBuildsJson
+status=$(jq '.status' $pagesBuildsJson | tr -d '"')
+echo $status
+
+if [ "$status" != "queued" ]
+then
+  exit 1
+fi
+
+pagesBuildsLatestUrl=$(jq '.url' $pagesBuildsJson | tr -d '"')
+pagesBuildsLatestJson="pages-builds-latest.json"
+
+rm $pagesBuildsJson
+
+while true
+do
+  sleep 5
+  curl -s -f -H "$authorization" -H "$accept" $pagesBuildsLatestUrl > $pagesBuildsLatestJson
+  status=$(jq '.status' $pagesBuildsLatestJson | tr -d '"')
+  echo $status
+  
+  if [ "$status" = "built" ]
+  then
+    rm $pagesBuildsLatestJson
+
+    exit
+  fi
+done
---
+++ readme.md
@@ -0,0 +1,12 @@
+# GitHub Pages
+
+Enqueues a GitHub Pages build and watches its building progress until the
+deployment is built.
+
+## Running
+
+```powershell
+ubuntu
+cd /mnt/c/Users/TomasHubelbauer/Desktop/github-pages
+./deploy.sh
+```
